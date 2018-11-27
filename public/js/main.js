61121fb422c7abca0e2434a8d2ff99c39fe3cabf
viv <l.vivian.93@gmail.com>
2018-11-27 19:05:28 +1100

    Remove mix grid loading animation because it was making the page jump.

--- public/js/main.js
+++ public/js/main.js
@@ -4,32 +4,7 @@ document.body.classList.add('js-loading');
 
 var elMixGrid = document.getElementById('portfolio-mix-grid');
 
-var mixer = mixitup(elMixGrid, {
-  animation: {
-    effects: 'scale stagger(50ms)' // Set a 'stagger' effect for the loading animation
-  },
-  load: {
-    filter: 'none' // Ensure all targets start from hidden (i.e. display: none;)
-  }
-});
-
-// Add a class to the container to remove 'visibility: hidden;' from targets. This
-// prevents any flicker of content before the page's JavaScript has loaded.
-elMixGrid.classList.add('mixitup-ready');
-
-// Show all targets in the container
-
-mixer.show()
-  .then(function() {
-    // Remove the stagger effect for any subsequent operations
-    mixer.configure({
-      animation: {
-        effects: 'scale',
-        nudge: 'true'
-      }
-    });
-  });
-
+var mixer = mixitup(elMixGrid);
 document.body.classList.remove('js-loading');
 
 /* Make filter scroll to top when they are clicked */
